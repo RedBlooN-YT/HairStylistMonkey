ad65d3167ca590ea9f2c21afad49d36b4aecb515
RedBlooN-YT <monkeyfiesta2@gmail.com>
2026-04-22 16:29:32 -0700

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,7 +2,7 @@ namespace HairStylistMonkey;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "54.1";
+    public const string WorksOnVersion = "54.2";
     public const string Version = "2.2.1";
     public const string Name = "HairStylistMonkey";
     public const string Description = "Adds Hair Stylist Monkey to the Game. Also adds toggles to add hair to bloons, towers, and or projectiles. This update adds more to the settings menu of the mod. There is much more added to the mod, with a slider that adds crazy effects to make this even more cursed. It also now updates as you change the settings if changing it from the in-game pause menu.";
